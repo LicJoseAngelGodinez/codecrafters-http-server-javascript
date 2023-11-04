bf7bd5c28dc3dc553c29969f78c4c0d4e2f320d1
Angel-Acer <licangelgodinez@hotmail.com>
2023-11-04 13:08:23 -0500

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -7,41 +7,62 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
-        const dataString = data.toString();
-        const [method, path, httpVersion, userAgent] = dataString.split(" ");
-        if (path === "/") {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-            socket.end();
-            return;
-        } else if (method === "GET" && path.startsWith('/echo/')) {
-            const randomString = path.substring(6);
-            socket.write("HTTP/1.1 200 OK\r\n");
-            socket.write("Content-Type: text/plain\r\n");
-            socket.write(`Content-Length: ${randomString.length}\r\n\r\n`);
-            socket.write(randomString);
-            console.log("Status 200 OK");
-            socket.end();
-            return;
-        } else if (method === "GET" && path.startsWith('/')) {
-            const userAgentData = userAgent.slice(0, userAgent.length-9);
-            socket.write("HTTP/1.1 200 OK\r\n");
-            socket.write("Content-Type: text/plain\r\n");
-            socket.write(`Content-Length: ${userAgentData.length}\r\n\r\n`);
-            socket.write(userAgentData);
-            console.log("Status 200 OK");
-            socket.end();
-            return;
+        const request = data.toString();
+        const requestDetails = parseRequest(request);
+        if (requestDetails.path.startsWith('/echo/')) {
+            const echoText = requestDetails.path.substring(6);
+            writeResponse(socket, 200, echoText);
+        } else if (requestDetails.path === '/user-agent') {
+            writeResponse(socket, 200, requestDetails.headers['userAgent']);
+        } else if (requestDetails.path === '/') {
+            writeResponse(socket, 200, 'OK');
         } else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-            socket.end();
-            return;
+            writeResponse(socket, 404, 'Not Found');
         }
+        socket.end();
+        socket.end();
   });
 
   socket.on("close", () => {
     socket.end();
+    socket.end();
+    server.close();
+  });
+  socket.on("error", () => {
+    socket.destroy();
+    socket.destroy();
     server.close();
   });
 });
 //
-server.listen(4221, "localhost");
+server.listen({
+    host: "localhost",
+    port: 4221,
+}, () => {
+    console.log('opened server on', server.address());
+});
+
+function parseRequest (request) {
+    const splitRemoveSpaces = request.split(/(\r\n|\n|\r)/gm).filter(line => line !== '' );
+    const headersSplit = splitRemoveSpaces.map(header => header.replace("\r\n", '')).filter(element => element !== '');
+    const [ method, path, httpVersion ] = headersSplit[0];
+    const headers = {
+        method,
+        path,
+        httpVersion
+    }
+
+    for ( i = 1; i <= headerss.length-1; i++ ) {
+        const [header, value] = headerss[i].split(': ')
+        headers[`${camelCase(header)}`] = value;
+    }
+    return headers;
+};
+
+// camelCase function credit to @apsillers 
+// https://stackoverflow.com/questions/10425287/convert-dash-separated-string-to-camelcase
+function camelCase(input) { 
+    return input.toLowerCase().replace(/-(.)/g, function(match, group1) {
+        return group1.toUpperCase();
+    });
+}
