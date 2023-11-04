336071e8dd3b887d54baef423af370d36389fde0
Angel-Acer <licangelgodinez@hotmail.com>
2023-11-04 11:14:21 -0500

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
         const dataString = data.toString();
-        const [method, path, httpVersion] = dataString.split(" ");
+        const [method, path, httpVersion, userAgent] = dataString.split(" ");
         if (path === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
             socket.end();
@@ -20,9 +20,21 @@ const server = net.createServer((socket) => {
             socket.write(`Content-Length: ${randomString.length}\r\n\r\n`);
             socket.write(randomString);
             console.log("Status 200 OK");
+            socket.end();
+            return;
+        } else if (method === "GET") {
+            const userAgentData = userAgent.slice(0, userAgent.length-9);
+            socket.write("HTTP/1.1 200 OK\r\n");
+            socket.write("Content-Type: text/plain\r\n");
+            socket.write(`Content-Length: ${userAgentData.length}\r\n\r\n`);
+            socket.write(userAgentData);
+            console.log("Status 200 OK");
+            socket.end();
+            return;
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
             socket.end();
+            return;
         }
   });
 
