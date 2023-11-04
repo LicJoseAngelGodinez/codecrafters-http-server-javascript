6131648cdc30084f81f9bceed03f77448a046b07
Angel-Acer <licangelgodinez@hotmail.com>
2023-11-04 10:22:17 -0500

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -8,14 +8,22 @@ const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
         const dataString = data.toString();
-        const [method, path] = dataString.split(" ");
-        if (path !== "/") {
-          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-          socket.end();
-          return;
+        const [method, path, httpVersion] = dataString.split(" ");
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            socket.end();
+            return;
+        } else if (method === "GET" && path.startsWith('/echo/')) {
+            const randomString = path.substring(6);
+            socket.write("HTTP/1.1 200 OK\r\n");
+            socket.write("Content-Type: text/plain\r\n");
+            socket.write(`Content-Length: ${randomString.length}\r\n\r\n`);
+            socket.write(randomString);
+            console.log("Status 200 OK");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            socket.end();
         }
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.end();
   });
 
   socket.on("close", () => {
